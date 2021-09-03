26778beedb6bb9f413ef6a2090b199963b098ebc
hashimovaturkan <turkansh@code.edu.az>
2021-09-03 12:09:56 +0400

    First Commit

---
+++ Riode/Riode Solution/Riode WebUI/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Riode_WebUI.Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Riode_WebUI.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Contact(Contact contact)
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+
+
+        public IActionResult FAQ()
+        {
+            return View();
+        }
+    }
+}
---
+++ Riode/Riode Solution/Riode WebUI/Models/Entities/BaseEntity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Riode_WebUI.Models.Entities
+{
+    public class BaseEntity
+    {
+        public long Id { get; set; }
+        public long? CreatedByUserId { get; set; }
+        public DateTime CreatedDate { get; set; } = DateTime.Now;
+        public long? DeletedByUserId { get; set; }
+        public DateTime? DeletedDate { get; set; }
+    }
+}
