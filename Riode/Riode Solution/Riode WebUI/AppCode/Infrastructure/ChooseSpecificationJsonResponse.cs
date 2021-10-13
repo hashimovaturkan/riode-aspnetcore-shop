b5af9df9905c9df9e35578d7aa8cdf69d327fdfd
hashimovaturkan <turkansh@code.edu.az>
2021-10-13 11:38:43 +0400

    update-13-10-2021

---
+++ Riode/Riode Solution/Riode WebUI/AppCode/Infrastructure/ChooseSpecificationJsonResponse.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Riode_WebUI.AppCode.Infrastructure
+{
+    public class ChooseSpecificationJsonResponse
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public bool Error { get; set; }
+    }
+}
--- Riode/Riode Solution/Riode WebUI/Areas/Admin/Controllers/ProductsController.cs
+++ Riode/Riode Solution/Riode WebUI/Areas/Admin/Controllers/ProductsController.cs
@@ -169,105 +169,8 @@ namespace Riode_WebUI.Areas.Admin.Controllers
             ViewData["CategoryId"] = new SelectList(await mediator.Send(new CategoryChooseQuery()), "Id", "Name", command.CategoryId);
 
             return View(command);
-            //    if (id != product.Id)
-            //    {
-            //        return NotFound();
-            //    }
-
-            //    if (ModelState.IsValid)
-            //    {
-            //        var entity =await db.Products
-            //            .Include(p=>p.Images.Where(pi=>pi.DeletedByUserId == null))
-            //            .FirstOrDefaultAsync(p => p.Id == product.Id && p.DeletedByUserId == null);
-            //        entity.Name = product.Name;
-            //        entity.Description = product.Description;
-
-            //        /// Silinmishler ---start
-            //        int[] deletedImageIds = product.Files.Where(p => p.Id > 0 && string.IsNullOrWhiteSpace(p.TempPath))
-            //                                        .Select(p => p.Id.Value)
-            //                                        .ToArray();
-
-            //        foreach (var itemId in deletedImageIds)
-            //        {
-            //            var oldImage = await db.ProductImages.FirstOrDefaultAsync(pi => pi.ProductId == entity.Id && pi.Id == itemId);
-
-            //            if (oldImage == null)
-            //                continue;
-
-            //            oldImage.DeletedDate = DateTime.Now;
-            //            oldImage.DeletedByUserId = 1;
-            //        }
-            //        /// Silinmishler ---end
-
-            //        foreach (var item in product.Files.Where(f=>(f.Id>0 && !string.IsNullOrWhiteSpace(f.TempPath)) //bazada olub deyismeyenler
-            //        || (f.File != null && f.Id ==null)))///yeni yarananlar
-            //        {
-            //            if(item.File == null) //deyismeyenler
-            //            {
-            //                var oldImage = await db.ProductImages.FirstOrDefaultAsync(pi => pi.ProductId == entity.Id && pi.Id == item.Id);
-
-            //                if (oldImage == null)
-            //                    continue;
-
-            //                oldImage.IsMain = item.IsMain;
-            //            }
-            //            else if(item.File != null) //yeni elave olunanlar
-            //            {
-            //                string extension = Path.GetExtension(item.File.FileName);
-            //                string fileName = $"{Guid.NewGuid()}{extension}";
-
-            //                string physicalFileName = Path.Combine(env.ContentRootPath,
-            //                                                       "wwwroot",
-            //                                                       "uploads",
-            //                                                       "images",
-            //                                                       "product",
-            //                                                       fileName);
-
-            //                using (var stream = new FileStream(physicalFileName, FileMode.Create, FileAccess.Write))
-            //                {
-            //                    await item.File.CopyToAsync(stream);
-            //                }
-
-            //                entity.Images.Add(new ProductImage
-            //                {
-            //                    FileName = fileName,
-            //                    IsMain = item.IsMain
-            //                });
-
-            //            }
-            //        }
-
-
-
-
-
-            //        await db.SaveChangesAsync();
-            //        return RedirectToAction(nameof(Index));
-            //    }
-            //    ViewData["BrandId"] = new SelectList(db.Brands, "Id", "Id", product.BrandId);
-            //    ViewData["CategoryId"] = new SelectList(db.Categories, "Id", "Id", product.CategoryId);
-            //    return View(product);
+            
         }
-
-        //[Authorize(Policy = "admin.products.delete")]
-        //public async Task<IActionResult> Delete(long? id)
-        //{
-        //    if (id == null)
-        //    {
-        //        return NotFound();
-        //    }
-
-        //    var product = await db.Products
-        //        .Include(p => p.Brand)
-        //        .Include(p => p.Category)
-        //        .FirstOrDefaultAsync(m => m.Id == id);
-        //    if (product == null)
-        //    {
-        //        return NotFound();
-        //    }
-
-        //    return View(product);
-        //}
 
         
         [HttpPost]
@@ -276,15 +179,19 @@ namespace Riode_WebUI.Areas.Admin.Controllers
         {
             var result = await mediator.Send(command);
             return Json(result);
-            //var product = await db.Products.FindAsync(id);
-            //db.Products.Remove(product);
-            //await db.SaveChangesAsync();
-            //return RedirectToAction(nameof(Index));
         }
 
-        //private bool ProductExists(long id)
-        //{
-        //    return db.Products.Any(e => e.Id == id);
-        //}
+        [HttpPost]
+        [Authorize(Policy = "admin.products.choose")]
+        public async Task<IActionResult> ChooseSpecification(ProductChooseSpecificationCommand command)
+        {
+            var result = await mediator.Send(command);
+            return Json(result);
+            
+        }
+
+
     }
+
+    
 }
